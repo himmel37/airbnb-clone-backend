b74f25a2ecc84bacac8e2b0aca8a31253653cf17
himmel37 <dahamong37@gmail.com>
2023-08-29 15:48:15 +0900

    custom houses

--- houses/admin.py
+++ houses/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import House
 
+
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    
     list_display = ("name", "price_per_night", "description", "address", "pets_allowed")
     list_filter = ("price_per_night", "pets_allowed")
     search_fields = ("name", "address")
