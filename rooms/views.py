1c10a7078af8607c12533658b9601cf8c4603851
himmel37 <dahamong37@gmail.com>
2023-09-20 22:07:55 +0900

    add status

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_204_NO_CONTENT
 from .models import Amenity
 from .serializer import AmenitySerializer
 
